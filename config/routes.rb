3b803e21923da3f5ea066d6fc9035256dadb0b50
Jan Jestaedt <jh.jestaedt@gmx.de>
2018-06-29 17:55:16 +0200

    final cocktail app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'pages/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cocktails, only: [ :index, :new, :create, :show ] do
     resources :doses, only: [:new, :create]
