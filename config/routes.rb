dfcf12a6a3e2317c9a3808e0c3e07d288f3cfe3a
Jan Jestaedt <jh.jestaedt@gmx.de>
2018-06-29 09:26:28 +0200

    Adds Home Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-  get 'reviews/create'
+  get 'pages/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cocktails, only: [ :index, :new, :create, :show ] do
     resources :doses, only: [:new, :create]
     resources :reviews, only: [:new, :create]
   end
+
+  root to: 'pages#home'
 
   resources :doses, only: [:destroy]
 end
